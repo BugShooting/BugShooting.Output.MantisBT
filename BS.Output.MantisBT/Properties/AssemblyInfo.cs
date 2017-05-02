77dcbd425ee42588beb0d15a11ab02245967e43f
Alexej <Alexej@ALEXEJ-PC>
2017-05-02 21:06:11 +0200

    first commit

---
+++ BS.Output.MantisBT/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BS.Output.MantisBT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bug Shooting")]
+[assembly: AssemblyCopyright("Copyright by Alexej Hirsch © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("72ae9e3b-42e3-444c-b460-f17a2cd6ad14")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
